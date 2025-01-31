5f614d9c5b41a28899ec17313976d2085f15b747
dshah2412 <shahdevanshi2412@gmail.com>
2025-01-31 16:04:51 +0530

    Devanshi Shah commit

---
+++ abc.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const abc = () => {
+  return (
+    <View>
+      <Text>abc</Text>
+    </View>
+  )
+}
+
+export default abc
+
+const styles = StyleSheet.create({})
